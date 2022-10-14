0b74f2dc05e88430b6fbf4c8edc56d2c56cbc863
hrushi-chavan <109643868+hrushi-chavan@users.noreply.github.com>
2022-10-14 13:18:44 +0530

    Update db_fetch.py

--- db_fetch.py
+++ db_fetch.py
@@ -1,19 +1,34 @@
+#To connect the SQL server and excute the sql query used pymysql lib
 import pymysql
+# to load the JSON file mysql_querry, which while read the database_server, User name, Password, Database name and data base port(3306) from mysql_querry file
 import json
+#To read and write the contain in the Excel file used openpyxl lib.
 from openpyxl import load_workbook
 
 def SelSqlFun():
     """" This function will fetch the information from table for certain critiria() """
+    
+    # used with clause to read the mysql_querry file and load the database_server, User name, Password, Database name and data base port in data dict object
     with open(r"mysql_querry", "r") as file:
         data = json.load(file)
+    
+    #used pymysql.connect method to connect the database using database_server, User name, Password, Database name and database port
     db = pymysql.connect(host=data["DB_HOST"], user=data["DB_USER"], password=data["DB_PASSWD"], database=data["DB_NAME"], port=data["DB_PORT"])
     cursor = db.cursor()
+    
+    # Build the sql query to fetch the details from database 
     sql = f'SELECT {data["DB_FIELD"]} FROM {data["TABLE_NAME"]} WHERE app_code IN ({data["APP_CODE"]});'
     try:
        # Execute the SQL command
        cursor.execute(sql)
+        
        # Fetch all the rows in a list of lists.
        results = cursor.fetchall()
+       
+       #Logic to fill the template using different type of servers, different type of Operating system or Database serevr
+       #Column 6(row[6]) for Type of server like PROD, DR, TEST, DEV 
+       # Column 3 (row[3]) for operating system type or database server type.
+       # depending upon the above value resplative template will filled. Like for PROD server and Linux operating system template1_pord_OS.xlsx will filled.
        for row in results:
            if ( row[6] == 'PROD' or row[6] == 'DR'):
                env="prod"
